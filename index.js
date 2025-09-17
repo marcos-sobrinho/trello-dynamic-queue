7fc17f6566d1ddf311301d8b5ac48f3f7f7bf114
marcos-sobrinho <marcospaniagosobrinho+github@gmail.com>
2025-09-17 18:45:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 /* index.js */
-var Promise = TrelloPowerUp.Promise;
-
-// Entry point: declare what this Power-Up does
 window.TrelloPowerUp.initialize({
-
-  // Add a button at the top of the board
-  'board-buttons': function (t, opts) {
+  'board-buttons': function(t, options) {
     return [{
       icon: 'https://marcos-sobrinho.github.io/trello-dynamic-queue/4133506.png',
-      text: 'Dynamic Queue',
-      callback: function (t) {
+      text: 'Open Popup',
+      callback: function(t) {
         return t.popup({
-          title: 'Dynamic Queue',
-          url: 'https://marcos-sobrinho.github.io/trello-dynamic-queue/popup.html', // opens your popup
-          height: 400
+          title: 'Popup Example',
+          url: 'https://marcos-sobrinho.github.io/trello-dynamic-queue/popup.html', // this must be listed in manifest.json
+          height: 200
         });
       }
     }];
   }
-
 });
-
